c58b9c8a4ab2616984dea502c4a69ed8deee8360
Z-K11 <zulqarnainbazidkhail@gmail.com>
2024-11-14 05:48:53 +0500

    Fixed typo 'trip amount' => 'tip_amount'

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ data = pd.read_csv('yellow_tripdata_2019-06.csv')
 # print(data.head())
 print("There are " + str(len(data)) + " observations in the dataset.")
 print("There are " + str(len(data.columns)) + " variables in the dataset.")
-data = data[data['trip_amount']>0]
-data = data[(data['trip_amount']<=data['fare_amount'])]
-data = data[((data['trip_amount']>=2)&(data['trip_amount']<200))]
+data = data[data['tip_amount']>0]
+data = data[(data['tip_amount']<=data['fare_amount'])]
+data = data[((data['tip_amount']>=2)&(data['tip_amount']<200))]
 cleanData = data.drop(['total_amount'],axis= 1)
 # The axis=1 parameter indicates that the drop should happen along columns (rather than rows).
 del data
 # frees the memory
 gc.collect()
 # is used to trigger manual garbage collection which involves clearing up unused objects and space hence freeing up memory
+print("There are " + str(len(cleanData)) + " observations in the dataset.")
+print("There are " + str(len(cleanData.columns)) + " variables in the dataset.")
