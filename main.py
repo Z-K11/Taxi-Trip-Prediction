32ef20b88d186ed3c995270f195efe08e5370c6a
Z-K11 <zulqarnainbazidkhail@gmail.com>
2024-11-14 10:11:08 +0500

    Convert tpep_pickup_datetime,tpep_dropoff_datetime to pandas date_time objects

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import warnings
 import gc,sys
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler,normalize,MinMaxScaler
-
+# Features
+# tpep_pickup_datetime,tpep_dropoff_datetime,passenger_count,trip_distance,RatecodeID,store_and_fwd_flag,PULocationID,DOLocationID,
+# payment_type,fare_amount,extra,mta_tax,tip_amount,tolls_amount,improvement_surcharge,total_amount,congestion_surcharge
 data = pd.read_csv('yellow_tripdata_2019-06.csv')
 # print(data.head())
 print("There are " + str(len(data)) + " observations in the dataset.")
@@ -27,3 +29,7 @@ plt.hist(cleanData.tip_amount.values,16,histtype="bar",facecolor='g')
 #plt.savefig("Visual_Representation.png")
 print("Minimum amount is ", np.min(cleanData.tip_amount.values))
 print("Maximum amount is ", np.max(cleanData.tip_amount.values))
+print("90% of the trips have a tip amount less or equal than :",np.percentile(cleanData.tip_amount.values,90))
+cleanData['tpep_pickup_datetime'] = pd.to_datetime(cleanData['tpep_pickup_datetime'])
+# converts the data into pandas date_time object
+cleanData["tpep_pickup_datetime"] = pd.to_datetime(cleanData['tpep_pickup_datetime'])
